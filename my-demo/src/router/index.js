b36ff0fe8f3e14c17e55bc1291555a5fea4870d5
lihailong@beinuniao.com <lihailong@beinuniao.com>
2017-06-05 18:52:16 +0800

    全家桶2

--- my-demo/src/router/index.js
+++ my-demo/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
+import demo from '@/components/demo'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: Hello
+    },
+    {
+      path:'/app',
+      name:'demo',
+      component:demo
     }
   ]
 })
