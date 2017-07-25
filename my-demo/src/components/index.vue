4129f1335606f23e2f49f30d07273ecf024f28b1
l5275238 <1043891613@qq.com>
2017-07-25 18:55:00 +0800

    改造

--- my-demo/src/components/index.vue
+++ my-demo/src/components/index.vue
@@ -9,6 +9,7 @@
         <div class="ivew">文章分类</div>
         <div class="ivew">文章管理</div>
     </div>
+    <div id="view"><router-view></router-view></div></div>
     <!--<div class="layout" :class="{'layout-hide-text': spanLeft < 5}">-->
       <!--<Row type="flex">-->
         <!--<i-col :span="spanLeft" class="layout-menu-left">-->
@@ -58,7 +59,6 @@
 
 
 
-  </div>
 </template>
 
 <script>
@@ -93,81 +93,146 @@ export default {
     }
   },
   mounted(){
-    var yuanArr=[];
     var canvas = document.getElementById('canvas');
     var width = canvas.offsetWidth;
     var height = canvas.offsetHeight;
+    var ctx = canvas.getContext('2d')
     canvas.width = width;
     canvas.height = height;
-    var ctx=canvas.getContext('2d')
-    function draw() {
-      ctx.fillRect(0,0,width,height);
-      ctx.beginPath();
-      var lingrad = ctx.createLinearGradient(0,-width,0,height);
-      lingrad.addColorStop(0, '#b7cbc4');
-      lingrad.addColorStop(1, '#dbc590');
-      ctx.fillStyle = lingrad;
-      ctx.fillRect(0,0,width,height);
-      var x=1000;
-      var y=1000;
-      for (var j=1;j<100;j++){
-        ctx.save();
-        x=width-Math.floor(Math.random()*width);
-        y=height-Math.floor(Math.random()*height)
-        var yuan=new Circle(x,y,ctx);
-        yuanArr.push(yuan);
-        yuan.draw(ctx);
-//            drawStar(ctx,0)
-        ctx.restore();
-      }
-    }
-    draw();
-    function Circle(x,y,content) {
-      this.x=x;
-      this.y=y;
-      this.r=5*Math.random();
-      this.aplin=0.8;
-      this.speed=1*1*Math.random()
-      this.draw=function () {
-//            content.strokeStyle='#fff';
-        this.aplin=this.y/height;
-        ctx.beginPath();
-        content.fillStyle='rgba(225,225,225,'+this.aplin+')';
-        content.save();
-        content.translate(this.x, this.y);
-//            content.fillStyle='red';
-//            content.beginPath();
-        content.arc(0,0,this.r,0,Math.PI*2,true)
-        content.closePath();
-        content.fill();
-        content.restore();
-      }
-      this.step=function () {
-        this.y=this.y-this.speed;
-        if(this.y<-this.r){
-          this.y=this.r+1000;
+    var pannel=new Panel(width,height,ctx);
 
+    //画布和线
+    function Panel(width,height,ctx) {
+      //速度X轴
+      var speedX=1;
+      //Y轴
+      var speedY=1;
+      //小球数量
+      var num=300;
+      线条半径
+      var r=100;
+      //小球的数组
+      this.round=[];
+      this.width=width;
+      this.height=height;
+      var lineX=width/2;
+      var lineY=height/2;
+      //移动线条的位置
+      this.move=function () {
+        if(lineX<0||lineX>this.width-200){
+          speedX=-speedX
+        }
+        else if(lineY<0||lineY>this.height-200){
+          speedY=-speedY
+        }
+        lineX+=speedX
+        lineY+=speedY
+      }
+      //画线条
+      this.DrawLine= function () {
+        var len=this.round.length;
+        for(var i = 0; i < len; i++){
+          for(var j = 0; j < len; j++){
+            var rv1=this.round[i];
+            var rv2=this.round[j];
+            if((rv1.x - rv2.x) <r && (rv1.y - rv2.y) < r && (rv1.x - rv2.x) > -r && (rv1.y - rv2.y) > - r){
+              if((rv1.x - lineX) < r && (rv1.y - lineY <  r && (rv1.x - lineX) > -  r && (rv1.y - lineY) > - r)){
+                ctx.beginPath();
+                ctx.strokeStyle = color();
+                ctx.moveTo(rv1.x, rv1.y);
+                ctx.lineTo(rv2.x, rv2.y);
+                ctx.stroke();
+                ctx.closePath();
+              }
+            }
+          }
         }
       }
+      //初始化画布
+      this.Initialization=function () {
+        ctx.clearRect(0, 0, this,width, this.height);
+        ctx.fillRect(0, 0, this,width, this.height);
+        ctx.beginPath();
+        var lingrad = ctx.createLinearGradient(0, -this.width, 0, this.height);
+        lingrad.addColorStop(0, '#000');
+        lingrad.addColorStop(1, '#002');
+        ctx.fillStyle = lingrad;
+        ctx.fillRect(0, 0, this.width, this.height);
+      }
+      //第一次画 将小球存放
+      this.draw=  function () {
+        this.Initialization();
+        for (var j = 1; j < num; j++) {
+          ctx.save();
+          var  x = this.width - Math.floor(Math.random() * this.width);
+          var  y = this.height - Math.floor(Math.random() * this.height)
+          var isFu=(j%2 == 0)?true:false;
+          var spot = new Circle(x, y, ctx,isFu);
 
-    }
-    function go() {
-      ctx.clearRect(0,0,width,height);
-      ctx.fillRect(0,0,width,height);
-      ctx.beginPath();
-      var lingrad = ctx.createLinearGradient(0,-width,0,height);
-      lingrad.addColorStop(0, '#b7cbc4');
-      lingrad.addColorStop(1, '#dbc590');
-      ctx.fillStyle = lingrad;
-      ctx.fillRect(0,0,width,height);
-      var len=yuanArr.length;
-      for(var i=0;i<len;i++){
-        var arr=yuanArr[i];
-        arr.step();
-        arr.draw();
+          this.round.push(spot)
+          spot.draw(ctx);
+          ctx.restore();
+        }
+      }
+      //循环画小球
+      this.loopDraw=function () {
+        var len = this.round.length;
+        for (var i = 0; i < len; i++) {
+          var arr = this.round[i];
+          arr.step();
+          arr.draw();
+        }
       }
     }
-    setInterval(go,10);
+    //随机颜色
+    function color() {
+      var g =  Math.floor(225 * Math.random());
+      var b =  Math.floor(225 * Math.random());
+      var a =  Math.floor(225 * Math.random());
+      var aplin = 1 * Math.random();
+      return 'rgba(' + g + ',' + b + ',' + a + ',' + aplin + ')'
+    }
+    //小点
+    function Circle(x, y, ctx,isFu) {
+      this.x = x;
+      this.y = y;
+      this.r = 3 * Math.random();
+      this.aplin = 0.8;
+      var  speedX =isFu? 1 * 1 * Math.random():-1 * 1 * Math.random();
+      var  speedY =isFu? 1 * 1 * Math.random():-1 * 1 * Math.random();
+      this.color = color();
+
+      this.draw = function () {
+        ctx.beginPath();
+        ctx.fillStyle = this.color;
+        ctx.save();
+        ctx.translate(this.x, this.y);
+        ctx.arc(0, 0, this.r, 0, Math.PI * 2, true)
+        ctx.closePath();
+        ctx.fill();
+        ctx.restore();
+      }
+      this.step = function () {
+        if (this.y <0||this.y>height) {
+          speedY=-speedY
+        }
+        else if(this.x <0||this.x>width){
+          speedX=-speedX
+        }
+        this.x+=speedX
+        this.y+=speedY
+      }
+    }
+    pannel.draw();
+    function go() {
+      pannel.Initialization()
+      pannel.move();
+      pannel.DrawLine();
+      pannel.loopDraw()
+
+      requestAnimationFrame(go);
+    }
+    requestAnimationFrame(go);
   }
 }
 </script>
@@ -239,8 +304,14 @@ export default {
   }
   #ivew{
     position: absolute;
-    width: 100%;
-    height: 100%;
+    /*width: 100%;*/
+    width: 200px;
+    text-align: center;
+    top:40%;
+    left: 0;
+    transform: translateY(-50%);
+    /*height: 100%;*/
+    z-index: 9999;
   }
   .ivew{
     width: 100px;
@@ -249,7 +320,22 @@ export default {
     background: #ffffff;
     text-align: center;
     line-height: 100px;
-    margin: 10px;
+    margin:50px;
     font-size: 14px;
+  }
+  .ivew:hover{
+    transform: translateX(10px);
+    width: 120px;
+    height: 120px;
+    line-height: 120px;
+    font-size:20px ;
+    transition: all 0.5s;
+  }
+  #view{
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    z-index: 999;
+    padding-left: 200px;
   }
 </style>
