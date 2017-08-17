947be6a61e604c919f01f36990c5d8be7b4d7274
l5275238 <1043891613@qq.com>
2017-08-17 18:12:30 +0800

    前台

--- my-demo2/src/App.vue
+++ my-demo2/src/App.vue
@@ -1,6 +1,49 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
+  <div id="app" >
+  <div class="container">
+    <div class="col-md-3">
+      <div class="nav-title">
+        Lee的博客
+      </div>
+      <ul class="list-group nav-list">
+        <li class="list-group-item "><img src="./assets/img/shouYe.png">首页</li>
+        <li class="list-group-item"><img src="./assets/img/guiDang.png">归档</li>
+        <li class="list-group-item"><img src="./assets/img/biaoQian.png">标签</li>
+        <li class="list-group-item"><img src="./assets/img/souSuo.png">搜索</li>
+      </ul>
+      <div class="nav-content">
+        <div class="nav-content-top">
+        <img src="./assets/logo.png">
+        <p>Lee</p>
+        <p>你好</p>
+        </div>
+        <div class="nav-content-middle">
+          <div class="col-md-4">
+            <p>11</p>
+            <p>日记</p>
+          </div>
+          <div class="col-md-4">
+            <p>6</p>
+            <p>分类</p>
+          </div>
+          <div class="col-md-4">
+            <p>111</p>
+            <p>标签</p>
+          </div>
+        </div>
+        <div class="nav-content-bottom">
+          <img src="./assets/img/github.png">
+          <a href="">GitHub</a>
+        </div>
+      </div>
+
+
+    </div>
+    <div class="col-md-9">
+
+    </div>
+
+  </div>
     <router-view></router-view>
   </div>
 </template>
@@ -18,6 +61,54 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  background: #f5f7f9;
+  min-height: 100%;
+  position: absolute;
+  width: 100%;
 }
+  .nav-title{
+    background: #000000;
+    color: #ffffff;
+    height: 150px;
+    line-height: 150px;
+    font-size: 20px;
+    font-weight: bold;
+  }
+  .nav-list{
+    padding: 20px 0;
+    background: #ffffff;
+  }
+
+  .list-group-item{
+    border: none;
+  }
+.nav-content{
+  background: #ffffff;
+  padding: 20px;
+}
+  .nav-content-top>img{
+    width: 150px;
+  }
+  .nav-content-top>p:nth-child(1){
+  font-weight: bold;
+  font-size: 20px;
+
+}
+.nav-content-top>p:nth-child(2){
+  font-weight: bold;
+  font-size: 20px;
+
+}
+  .nav-content-middle{
+    overflow: hidden;
+  }
+  .nav-content-middle div:nth-child(1){
+       border-right: 2px solid #cccccc;
+     }
+.nav-content-middle div:nth-child(2){
+  border-right: 2px solid #cccccc;
+}
+  .nav-content-bottom{
+    padding: 10px ;
+  }
 </style>
